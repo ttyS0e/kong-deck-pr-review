e939607a9d72bafa2f18b37a5a54094bb6dd1a5e
Jack Tysoe <jack.tysoe@konghq.com>
2022-07-27 12:26:42 +0100

    test

--- empty-plugins-generate.sh
+++ empty-plugins-generate.sh
@@ -5,9 +5,9 @@ for API_SPEC in ./api/*.yaml; do
 
     # Create the sample service-level plugin
     API_NAME=$(yq e '.x-kong-name' api/$API_FILE)
-    echo -e "_format_version: \"1.1\"\n_workspace: \"rb\"\n_info:\n  select_tags:\n  - sample-api\n\nplugins:\n- service: \"$API_NAME\"\n  name: {plugin_name}\n  config:\n    option_1: val\n    option_2: val\n" > plugins/$API_FILE
+    echo -e "_format_version: \"1.1\"\n_workspace: \"rb\"\n_info:\n  select_tags:\n  - sample-api\n\n# plugins:\n# - service: \"$API_NAME\"\n#   name: {plugin_name}\n#   config:\n#     option_1: val\n#     option_2: val" > plugins/$API_FILE
 
     for OP_ID in $(yq e '.paths.*.*."x-kong-name"' api/echo-server.yaml); do
-        echo -e "- route: \"$OP_ID\"\n  name: {plugin_name}\n  config:\n    option_1: val\n    option_2: val\n" >> plugins/$API_FILE
+        echo -e "# - route: \"$OP_ID\"\n#   name: {plugin_name}\n#   config:\n#     option_1: val\n#     option_2: val" >> plugins/$API_FILE
     done
 done
